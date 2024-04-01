ba448a9d4606f840d28f1c0f72a2931cf96b5d6f
Fran Bec <franbecvort@gmail.com>
2024-04-01 18:10:36 +0100

    returning an object to simplify work at microservice-a

--- src/main/java/dev/pollito/microserviceb/controller/DefaultController.java
+++ src/main/java/dev/pollito/microserviceb/controller/DefaultController.java
@@ -1,13 +1,14 @@
 package dev.pollito.microserviceb.controller;
 
 import dev.pollito.microserviceb.api.DefaultApi;
+import dev.pollito.microserviceb.models.HelloWorld;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DefaultController implements DefaultApi {
   @Override
-  public ResponseEntity<String> helloWorld() {
-    return ResponseEntity.ok("Hello World from Microservice B");
+  public ResponseEntity<HelloWorld> helloWorld() {
+    return ResponseEntity.ok(new HelloWorld().content("Hello world from microservice B"));
   }
 }
